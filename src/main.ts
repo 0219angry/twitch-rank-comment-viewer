de6823b16cd2ba65a682f6b13b8f14e630db258b
TsuyukusaYu <angry.yuma.0219@gmail.com>
2025-01-26 17:24:41 +0900

    起動するようになった

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,28 @@
-import { BrowserWindow, app, ipcMain, IpcMainInvokeEvent } from 'electron'
+import { BrowserWindow, app, ipcMain, IpcMainInvokeEvent } from 'electron';
+import path from 'path';
 
+let mainWindow: BrowserWindow | null;
+
+// BrowserWindowのインスタンス生成
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 320,
+    height: 640,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    }
+  });
+  console.log(__dirname)
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+
+app.once('ready', () => {
+  createWindow();
+});
+
+app.once('window-all-closed', () => {
+  app.quit();
+});
